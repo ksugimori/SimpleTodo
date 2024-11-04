97645cd7f5639fa5f52fe2d79af992e521fd6217
ksugimori <ksugimori.private@gmail.com>
2024-11-04 17:46:53 +0900

    Add complete method

---
+++ src/main/java/com/example/simple_todo/presentation/form/TaskCompleteForm.java
@@ -0,0 +1,13 @@
+package com.example.simple_todo.presentation.form;
+
+public class TaskCompleteForm {
+    private Long taskId;
+
+    public Long getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(Long taskId) {
+        this.taskId = taskId;
+    }
+}
