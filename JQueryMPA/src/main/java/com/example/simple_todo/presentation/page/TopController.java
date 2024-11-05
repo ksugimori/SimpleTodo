147ff6321ba5002b71c38012cb6649cd351b8b5d
ksugimori <12605906+ksugimori@users.noreply.github.com>
2024-11-05 22:42:46 +0900

    初期表示もJSで描画するように変更

--- JQueryMPA/src/main/java/com/example/simple_todo/presentation/page/TopController.java
+++ JQueryMPA/src/main/java/com/example/simple_todo/presentation/page/TopController.java
@@ -1,13 +1,8 @@
 package com.example.simple_todo.presentation.page;
 
-import com.example.simple_todo.domain.Task;
 import com.example.simple_todo.domain.TaskRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Comparator;
-import java.util.List;
 
 @Controller
 public class TopController {
@@ -18,14 +13,7 @@ public class TopController {
     }
 
     @GetMapping("/")
-    public String index(Model model) {
-        List<Task> tasks = taskRepository.findAll()
-                .stream()
-                .sorted(Comparator.comparing(Task::id))
-                .toList();
-
-        model.addAttribute("tasks", tasks);
-
+    public String index() {
         return "index";
     }
 }
