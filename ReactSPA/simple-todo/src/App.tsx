8842b09b4e8e0e680aadef962714313954e5536e
ksugimori <12605906+ksugimori@users.noreply.github.com>
2024-11-10 10:56:35 +0900

    Add event handlers

--- ReactSPA/simple-todo/src/App.tsx
+++ ReactSPA/simple-todo/src/App.tsx
@@ -1,13 +1,16 @@
+import { useState } from 'react'
 import './App.css'
 import TaskTable from './components/tasktable/TaskTable'
 import Task from './types/Task'
 
-const tasks: Task[] = [
-  { id: 1, subject: "掃除する", isCompleted: false },
-  { id: 2, subject: "洗濯する", isCompleted: true }
+const initialState: Task[] = [
+  Task.create("掃除する").complete(),
+  Task.create("洗濯する"),
 ]
 
 function App() {
+  const [tasks, setTasks] = useState(initialState);
+
   return (
     <main>
       <h1>Simple TODO</h1>
@@ -18,7 +21,18 @@ function App() {
       </form>
 
       <h2>一覧</h2>
-      <TaskTable tasks={tasks} />
+      <TaskTable
+        tasks={tasks}
+        onComplete={id => {
+          console.log(`id = ${id}`);
+          const result = tasks.map(task => (task.id === id ? task.complete() : task))
+          setTasks(result);
+        }}
+        onDelete={id => {
+          const result = tasks.filter(task => task.id !== id);
+          setTasks(result);
+        }}
+      />
 
     </main>
   )
